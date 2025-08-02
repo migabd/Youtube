be1e488433d38635e4bdeccf1c7eafcc765f8f0c
migabd <151981183+migabd@users.noreply.github.com>
2025-08-02 12:46:17 -0300

    Delete BlockPornAdguard.bat

--- BlockPornAdguard.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: Executa o PowerShell como administrador para alterar o DNS
-powershell -Command "Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -NoProfile -Command \"& {
-$interface = Get-NetAdapter | Where-Object { $_.Status -eq \"Up\" } | Select-Object -First 1 -ExpandProperty Name;
-Set-DnsClientServerAddress -InterfaceAlias $interface -ServerAddresses (\"94.140.14.14\", \"94.140.15.15\");
-}\"' -Verb RunAs"
-pause
